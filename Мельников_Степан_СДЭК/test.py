827df3fda536656611ba79dde5be0e346afafb09
Stepan <stepan.melnickov.ne@yandex.ru>
2022-10-14 01:10:22 +0500

    Начало работы над MyEssay.md и добавлена 2 версия работы программы

--- Мельников_Степан_СДЭК/test.py
+++ Мельников_Степан_СДЭК/test.py
@@ -1,1 +1,5 @@
-print(' '.join(map(lambda x: x.capitalize(), input().replace('  ', ' ').split(' '))))
+# print(' '.join(map(lambda x: x.capitalize(), input().replace('  ', ' ').split(
+#     ' '))))  # Здесь первая буква становится заглавной, а все остальные строчными
+
+print(' '.join(map(lambda x: x[0].upper() + x[1:], input().replace('  ', ' ').split(
+    ' '))))  # В данном варианте только первая буква становится заглавной, остальные без изменений
