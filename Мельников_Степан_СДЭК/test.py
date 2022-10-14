f0ea50cd5d9818949861e41e4b9495dc1d72accf
Stepan <stepan.melnickov.ne@yandex.ru>
2022-10-14 22:55:54 +0500

    Я понял что сделал неправильно

--- Мельников_Степан_СДЭК/test.py
+++ Мельников_Степан_СДЭК/test.py
@@ -1,5 +1,5 @@
 # print(' '.join(map(lambda x: x.capitalize(), input().replace('  ', ' ').split(
 #     ' '))))  # Здесь первая буква становится заглавной, а все остальные строчными
 
-print(' '.join(map(lambda x: x[0].upper() + x[1:], input().replace('  ', ' ').split(
+print(' '.join(map(lambda x: x[0].upper() + x[1:] if x else '', input().split(
     ' '))))  # В данном варианте только первая буква становится заглавной, остальные без изменений
