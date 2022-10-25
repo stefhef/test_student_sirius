b0085323768bbcaed8ed46c03dd4e26caabbc6b3
Stepan Melnickov <stepan.melnickov.ne@yandex.ru>
2022-10-25 17:19:15 +0500

    Хехеххехехе

--- Мельников_Степан_СДЭК/test.py
+++ Мельников_Степан_СДЭК/test.py
@@ -1,5 +1,7 @@
 # print(' '.join(map(lambda x: x.capitalize(), input().replace('  ', ' ').split(
 #     ' '))))  # Здесь первая буква становится заглавной, а все остальные строчными
 
-print(' '.join(map(lambda x: x[0].upper() + x[1:] if x else '', input().split(
-    ' '))))  # В данном варианте только первая буква становится заглавной, остальные без изменений
+# print(' '.join(map(lambda x: x[0].upper() + x[1:] if x else '', input().split(
+#     ' '))))  # В данном варианте только первая буква становится заглавной, остальные без изменений
+
+print(input().title())
